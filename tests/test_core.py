2e35f3a1b89e230a754feb578b3049f041ec55ca
Kevin Wurster <kevin@skytruth.org>
2015-06-01 12:47:11 -0400

    Let user sum a field rather than just computing density.  Closes #5.

--- tests/test_core.py
+++ tests/test_core.py
@@ -17,14 +17,16 @@ def test_standard_with_res_and_shape():
             f.seek(0)
             args = [
                 '--i-drv', 'NewlineJSON',
+                '--i-cmp', 'GZIP',
                 'density',
-                'tests/data/messages1.json',
-                'tests/data/messages2.json',
+                'tests/data/messages1.json.gz',
+                'tests/data/messages2.json.gz',
                 '-c', 'COMPRESS=DEFLATE',
                 '-c', 'PREDICTOR=2',
                 '-c', 'ZLEVEL=9',
                 '-c', 'BIGTIFF=NO',
                 '-c', 'TILED=YES',
+                '-c', 'INTERLEAVE=BAND',
                 '--dtype', 'Int16',
                 f.name
             ]
@@ -43,3 +45,33 @@ def test_standard_with_res_and_shape():
                 # If the affine or transform is not properly written then
                 # one of these will be incorrect.  Should have world extent.
                 assert expected.bounds == actual.bounds == (-180, -90, 180, 90)
+
+
+def test_with_field():
+
+    with tempfile.NamedTemporaryFile('r+') as f:
+        result = CliRunner().invoke(gpsdio.cli.main.main_group, [
+            'density',
+            'tests/data/messages1.json.gz',
+            'tests/data/messages2.json.gz',
+            '--res', '10',
+            '--field', 'val',
+            '--dtype', 'Int16',
+            '-c', 'COMPRESS=DEFLATE',
+            '-c', 'PREDICTOR=2',
+            '-c', 'ZLEVEL=9',
+            '-c', 'BIGTIFF=NO',
+            '-c', 'TILED=YES',
+            '-c', 'INTERLEAVE=BAND',
+            f.name
+        ])
+        f.seek(0)
+        print(result.output)
+        assert result.exit_code is 0
+
+        with rio.open(f.name) as actual, \
+                rio.open('tests/data/test-with-field-expected.tif') as expected:
+
+            assert actual.meta == expected.meta
+            assert actual.read(indexes=1).all() * 2 == expected.read(indexes=1).all()
+            assert expected.bounds == actual.bounds == (-180, -90, 180, 90)
